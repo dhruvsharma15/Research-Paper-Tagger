12c419b17151e76f687da45fde80737d62b82d5e
Naman Ahuja <naman@live.in>
2018-10-12 11:42:56 -0400

    download papers final commit

--- downloadPapers.py
+++ downloadPapers.py
@@ -8,8 +8,8 @@ import requests
 with open('arxivData.json', 'r') as f:
     papers = json.load(f)
  
-papers = papers[0:1000]
 i = 0
+papers = papers[0:1000]
 
 for paper in papers:
     i = i + 1
@@ -17,7 +17,7 @@ for paper in papers:
     link = paper['link']
     link = link.replace("'", '"')
     link = json.loads(link)
-    file_url = link[1]['href']
+    file_url = link[-1]['href']
     print(file_url)
     r = requests.get(file_url, stream = True) 
     with open("file" + str(i) + ".pdf","wb") as pdf: 
